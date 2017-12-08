7cf8866026e28c6dc09e4f309dd7c6c9ef6111e1
didier <didier@d-defrance.fr>
2017-12-08 11:16:43 +0100

    avec .bat

--- s_sync_push_toGitHub.bat
+++ s_sync_push_toGitHub.bat
@@ -1,4 +1,7 @@
+
+git remote remove GitHubMyContribOrigin
 git remote add GitHubMyContribOrigin https://github.com/didier-mycontrib/tp_angular4plus.git
+
 REM didier-mycontrib / gh14.....sm..x / didier@d-defrance.fr
 git push -u GitHubMyContribOrigin master
 pause
